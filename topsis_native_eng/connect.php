b2a066e27c52ad3b9105b61b3970aac412d24321
WebIoT.id <34671621+ahroihan@users.noreply.github.com>
2022-08-12 20:02:09 +0700

    Update connect.php

--- topsis_native_eng/connect.php
+++ topsis_native_eng/connect.php
@@ -1,5 +1,5 @@
 <?php
-	error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
+    error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
     date_default_timezone_set('America/Los_Angeles');
     ini_set('max_execution_time', 0);
     session_start();
